623fa9d45985cc892252020bc1b217a39046b3ae
Stephen Chang <stchang@ccs.neu.edu>
2013-09-09 17:47:32 -0400

    update space experiments file

--- generic-bind/experimenting-with-space-usage.rkt
+++ generic-bind/experimenting-with-space-usage.rkt
@@ -1,10 +1,29 @@
 #lang racket
-(let loop ([s (stream-filter identity (in-range 10000000))] [sum 0]) 
+(require "generic-bind.rkt")
+
+#;(let loop ([s (stream-filter identity (in-range 1000000))] [sum 0]) 
     (if (stream-empty? s) sum 
         (loop 
          (stream-rest s) 
-         (let loop2 ([t (stream-filter identity (in-range 1))] [sum sum]) 
+         (let loop2 ([t (stream-filter identity (in-range 10))] [sum sum]) 
            (if (stream-empty? t) sum 
                (loop2 
                 (stream-rest t) 
                 (+ sum (stream-first s) (stream-first t))))))))
+
+(let ([ss (stream-filter identity (in-range 10000000))])
+  (let loop ([s ss] [sum 0])
+    (if (stream-empty? s) sum 
+        (loop 
+         (stream-rest s) 
+         (+ sum (stream-first s))
+         #;(let loop2 ([t (stream-filter identity (in-range 10))] [sum sum]) 
+           (if (stream-empty? t) sum 
+               (loop2 
+                (stream-rest t) 
+                (+ sum (stream-first s) (stream-first t)))))))))
+
+#;(~for/sum ([s (stream-filter identity (in-range 10000000))]
+          #:when #t
+          #;[t (stream-filter identity (in-range 1))])
+  s)
