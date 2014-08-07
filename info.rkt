b8ade24fcfea9ded5fce7914df7ff98eaa3eda75
AlexKnauth <alexander@knauth.org>
2014-08-06 21:37:08 -0400

    Add undeclared dependancies

--- info.rkt
+++ info.rkt
@@ -4,4 +4,4 @@
 ;; and declare the dependency on racket, not base
 ;; see: https://www.mail-archive.com/users@racket-lang.org/msg23164.html
 (define deps '(("base" #:version "5.90.0.9")))
-(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib"))
+(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib" "math-lib" "compatibility-lib"))
